7dc34ccf84859d55899c232a1dc4b63e8657e839
Vukovi <Vuk@Users-MacBook-Pro.local>
2017-03-13 21:50:42 +0100

    @IBOutlets

--- Pokemon/PokemonDetailVC.swift
+++ Pokemon/PokemonDetailVC.swift
@@ -12,12 +12,29 @@ class PokemonDetailVC: UIViewController {
     
     var pokemon: Pokemon!
     @IBOutlet weak var nameLabel: UILabel!
-
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexIDLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var baseAttackLabel: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var extEvoImage: UIImageView!
+    @IBOutlet weak var evoLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        nameLabel.text = pokemon.name
+        nameLabel.text = pokemon.name.capitalized
     }
 
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
